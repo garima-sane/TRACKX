14a301b562f7e5fd316c6e58b1184b9837f54b6b
sane <222829143+GaRiMa-0019@users.noreply.github.com>
2026-07-16 22:27:16 +0530

    Refactor data pipeline by moving hit_song creation to cleaning stage

--- trackx_ml.py
+++ trackx_ml.py
@@ -21,7 +21,20 @@ print("\nDataset Columns:", df.columns)
 print("\n first 5 rows of the dataset:\n", df.head())
 print("\n Missing Values in the dataset:\n", df.isnull().sum())
 
-audio_features = ['danceability', 'energy', 'loudness', , 'speechiness',
+audio_features = ['danceability', 'energy', 'loudness', 'speechiness',
                   'acousticness', 'instrumentalness', 'liveness', 'valence',
-                  'tempo']
+                  'tempo', 'duration_ms']
+
+#Creating Labels and Features
+
+x=df[audio_features]
+y=df["hit_song"]
+x_train, x_test, y_train, y_test = train_test_split(
+                                                    x, 
+                                                    y, 
+                                                    test_size=0.2, 
+                                                    random_state=47)   
+
+print("\nTraining Set Shape:", x_train.shape, y_train.shape)
+print("\nTesting Set Shape:", x_test.shape, y_test.shape)
 
