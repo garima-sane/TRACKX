e5475ec311d5b4b11495589db051df4810fad32b
sane <222829143+GaRiMa-0019@users.noreply.github.com>
2026-07-17 01:46:39 +0530

    Implement and compare Logistic Regression, Decision Tree, and Random Forest models

--- trackx_ml.py
+++ trackx_ml.py
@@ -5,6 +5,12 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 
 # Machine Learning Libraries
 from sklearn.model_selection import train_test_split
@@ -38,3 +44,56 @@ x_train, x_test, y_train, y_test = train_test_split(
 print("\nTraining Set Shape:", x_train.shape, y_train.shape)
 print("\nTesting Set Shape:", x_test.shape, y_test.shape)
 
+#==============================
+#Model Training
+#==============================
+
+#logistic regression model (balanced class and baseline class) #
+lr_model = LogisticRegression( max_iter=1000)
+lr_model.fit(x_train, y_train)
+print("\nLogistic Regression Model Training Completed Successfully.")
+
+lrb_model = LogisticRegression(class_weight='balanced', max_iter=1000)
+lrb_model.fit(x_train, y_train)
+print("\nBalanced Logistic Regression Model Training Completed Successfully.")
+
+# Decision Tree Model #
+dt_model = DecisionTreeClassifier(random_state=47)
+dt_model.fit(x_train, y_train)
+print("\nDecision Tree Model Training Completed Successfully.")
+
+# Random Forest Model #
+rf_model = RandomForestClassifier(random_state=47)
+rf_model.fit(x_train, y_train)
+print("\nRandom Forest Model Training Completed Successfully.")
+
+#==============================
+#Model Evaluation
+#==============================
+
+lr_pred = lr_model.predict(x_test)
+lrb_pred = lrb_model.predict(x_test)
+tree_pred = dt_model.predict(x_test)
+
+#==============================
+#Model Accuracy, Precision, Recall, F1-Score
+#==============================
+
+#Accuracy 
+lr_accuracy= accuracy_score(y_test, lr_pred)
+print(f"\n Logistic Regression Model Accuracy: {lr_accuracy:.3f}")
+
+lrb_accuracy = accuracy_score(y_test, lrb_pred)
+print(f"\n Balanced Logistic Regression Model Accuracy: {lrb_accuracy:.3f}")
+
+tree_accuracy = accuracy_score(y_test, tree_pred)
+print(f"\nDecision Tree Model Accuracy: {tree_accuracy:.3f}")
+
+forest_accuracy = accuracy_score(y_test, rf_model.predict(x_test))
+print(f"\nRandom Forest Model Accuracy: {forest_accuracy:.3f}")
+
+#Classification Report
+print("\nLogistic Regression Classification Report:\n", classification_report(y_test, lr_pred))
+print("\nBalanced Logistic Regression Classification Report:\n", classification_report(y_test, lrb_pred))
+print("\nDecision Tree Classification Report:\n", classification_report(y_test, tree_pred))
+print("\nRandom Forest Classification Report:\n", classification_report(y_test, rf_model.predict(x_test)))
